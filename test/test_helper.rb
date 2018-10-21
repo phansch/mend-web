1793f241fa20adde3bb8b3c70b6e39ea78d04063
Philipp Hansch <dev@phansch.net>
2018-10-21 09:34:40 +0200

    Remove database_cleaner gem
    I will try to just use fixtures and hopefully won't have to deal with
    a non-emtpy database during tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,30 +21,22 @@ require 'webmock/minitest'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-DatabaseCleaner.strategy = :transaction
-DatabaseCleaner.clean_with(:truncation)
-
 module AroundEachTest
   def before_setup
-    DatabaseCleaner.start
     OmniAuth.config.test_mode = true
     super
-  end
-
-  def after_teardown
-    super
-    DatabaseCleaner.clean
   end
 end
 
 module ActiveSupport
   class TestCase
-    include AroundEachTest
-
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+
+    include AroundEachTest
   end
 end
+
 module ActionDispatch
   class IntegrationTest
     include AroundEachTest
