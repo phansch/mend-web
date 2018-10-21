f3220e2a19965fa2735479a34335525ad5afb9e9
Philipp Hansch <dev@phansch.net>
2018-10-21 15:12:12 +0200

    Add installations dashboard

---
+++ app/dashboards/installation_dashboard.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
+
+class InstallationDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    user: Field::BelongsTo,
+    id: Field::Number,
+    external_id: Field::Number,
+    access_token: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    user
+    id
+    external_id
+    access_token
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    user
+    id
+    external_id
+    access_token
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    user
+    external_id
+    access_token
+  ].freeze
+
+  # Overwrite this method to customize how installations are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(installation)
+  #   "Installation ##{installation.id}"
+  # end
+end
