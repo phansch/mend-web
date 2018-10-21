6580bdc8c55263d7ad72260207b304eccb1740b4
Philipp Hansch <dev@phansch.net>
2018-10-21 15:04:13 +0200

    Save user with installation

--- test/models/installation_test.rb
+++ test/models/installation_test.rb
@@ -6,9 +6,10 @@ class InstallationTest < ActiveSupport::TestCase
   test '.create_from_installation_id' do
     client_mock = Minitest::Mock.new
     client_mock.expect :create_app_installation_access_token, { token: 'some_token' }, [1]
+    user = users(:one)
     Octokit::Client.stub(:new, client_mock) do
       assert_difference -> { Installation.count }, 1 do
-        Installation.create_from_installation_id(1)
+        Installation.create_from_installation_id(1, user)
       end
       assert_equal 1, Installation.last.external_id
       assert_equal 'some_token', Installation.last.access_token
